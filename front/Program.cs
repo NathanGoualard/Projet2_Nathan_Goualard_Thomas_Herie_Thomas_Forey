4419a4b7c6ebff93b8cbc11cb85f6a06035f56e7
Seremptos <thomasf60120@gmail.com>
2026-01-06 19:19:27 +0100

    Page de connexion

--- front/Program.cs
+++ front/Program.cs
@@ -25,5 +25,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+app.MapControllerRoute(
+    name: "login",
+    pattern:"{controller=Login}/{action=Login}"
+).WithStaticAssets();
+
 
 app.Run();
